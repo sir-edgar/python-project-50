b56773ef560618932eaf50eda8332826d72bc8c6
Timur Vitrov <timurvitrov@gmail.com>
2024-09-14 18:30:34 +0300

    step 6

--- gendiff/modules/difference.py
+++ gendiff/modules/difference.py
@@ -1,23 +1,21 @@
 import json
+from gendiff.modules.parser import parser
 
 
 def generate_diff(file_path1, file_path2):
-    dict1 = json.load(open(file_path1))
-    dict2 = json.load(open(file_path2))
-    set1 = set(dict1)
-    set2 = set(dict2)
+    set1, set2, dict1, dict2 = parser(file_path1, file_path2)
     sorted_union = sorted(list(set1.union(set2)))
-    result = ''
+    result = '{\n'
     for key in sorted_union:
         if key in dict1 and key in dict2:
             if dict1.get(key) == dict2.get(key):
-                result = result + f'  {key}: {str(dict1.get(key))}' + '\n'
+                result = result + f'    {key}: {str(dict1.get(key))}' + '\n'
             else:
-                result = result + f'- {key}: {str(dict1.get(key))}' + '\n'
-                result = result + f'+ {key}: {str(dict2.get(key))}' + '\n'
+                result = result + f'  - {key}: {str(dict1.get(key))}' + '\n'
+                result = result + f'  + {key}: {str(dict2.get(key))}' + '\n'
         elif key in dict1 or key in dict2:
             if key in dict1:
-                result = result + f'- {key}: {str(dict1.get(key))}' + '\n'
+                result = result + f'  - {key}: {str(dict1.get(key))}' + '\n'
             else:
-                result = result + f'+ {key}: {dict2[key]}' + '\n'
-    return result
+                result = result + f'  + {key}: {dict2[key]}' + '\n'
+    return result + '}'
--- tests/test_difference.py
+++ tests/test_difference.py
@@ -1,11 +1,24 @@
 from gendiff.modules.difference import generate_diff
 
 
-def test_diff():
-    assert generate_diff('gendiff/file1.json',
-                         'gendiff/file2.json') == ('- follow: False\n'
-                                                   '  host: hexlet.io\n'
-                                                   '- proxy: 123.234.53.22\n'
-                                                   '- timeout: 50\n'
-                                                   '+ timeout: 20\n'
-                                                   '+ verbose: True\n')
+def test_diff_json():
+    assert generate_diff('/Users/timurvitrov/python-project-50/gendiff/file1.json',
+                         '/Users/timurvitrov/python-project-50/gendiff/file2.json') == ('{\n'
+                                                                                        '  - follow: False\n'
+                                                                                        '    host: hexlet.io\n'
+                                                                                        '  - proxy: 123.234.53.22\n'
+                                                                                        '  - timeout: 50\n'
+                                                                                        '  + timeout: 20\n'
+                                                                                        '  + verbose: True\n'
+                                                                                        '}')
+
+def test_diff_yaml():
+    assert generate_diff('/Users/timurvitrov/python-project-50/gendiff/file1.yaml',
+                         '/Users/timurvitrov/python-project-50/gendiff/file2.yaml') == ('{\n'
+                                                                                        '  - follow: False\n'
+                                                                                        '    host: hexlet.io\n'
+                                                                                        '  - proxy: 123.234.53.22\n'
+                                                                                        '  - timeout: 50\n'
+                                                                                        '  + timeout: 20\n'
+                                                                                        '  + verbose: True\n'
+                                                                                        '}')
