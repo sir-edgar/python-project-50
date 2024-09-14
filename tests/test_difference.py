828d768ed43343c760e198098336262c718935da
Timur Vitrov <timurvitrov@gmail.com>
2024-09-14 19:00:55 +0300

    14.09

--- tests/test_difference.py
+++ tests/test_difference.py
@@ -2,8 +2,8 @@ from gendiff.modules.difference import generate_diff
 
 
 def test_diff_json():
-    assert generate_diff('/Users/timurvitrov/python-project-50/gendiff/file1.json',
-                         '/Users/timurvitrov/python-project-50/gendiff/file2.json') == ('{\n'
+    assert generate_diff('/Users/timurvitrov/python-project-50/tests/tests/fixtures/file1.json',
+                         '/Users/timurvitrov/python-project-50/tests/tests/fixtures/file2.json') == ('{\n'
                                                                                         '  - follow: False\n'
                                                                                         '    host: hexlet.io\n'
                                                                                         '  - proxy: 123.234.53.22\n'
@@ -13,8 +13,8 @@ def test_diff_json():
                                                                                         '}')
 
 def test_diff_yaml():
-    assert generate_diff('/Users/timurvitrov/python-project-50/gendiff/file1.yaml',
-                         '/Users/timurvitrov/python-project-50/gendiff/file2.yaml') == ('{\n'
+    assert generate_diff('/Users/timurvitrov/python-project-50/tests/tests/fixtures/file1.yaml',
+                         '/Users/timurvitrov/python-project-50/tests/tests/fixtures/file2.yaml') == ('{\n'
                                                                                         '  - follow: False\n'
                                                                                         '    host: hexlet.io\n'
                                                                                         '  - proxy: 123.234.53.22\n'
